ddfee131b6c9f9947324d1a0f7b1889cd6ac7efe
playtime139 <ryankimungui@gmail.com>
2025-11-29 14:59:24 +1100

    pass.py

---
+++ pass.py
@@ -0,0 +1,11 @@
+for x in range(10):
+    if x % 15 == 0:
+        print("oreo")
+
+    elif x % 6 == 0:
+        pass
+    
+    elif x % 7 == 0:
+        print("stuff")
+    else:
+        print(x)
